8e49d20b97ffe4fc141ba50ee6519f72782b6954
AnastasiyaShvdko <m.a.34@yandex.ru>
2021-05-22 14:28:26 +0300

    Said Hello

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello my name is Jurgen.");
+    System.out.println("Hello Jurgen, my name is Nastya.");
     }
 }
